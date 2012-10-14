6d0f40f3272cc206b72f07db4be6e0003c41ddde
Pedro Algarvio <pedro@algarvio.me>
2012-10-14 23:42:50 +0100

    Allow some more time before failing because of bad machine performance.

--- tests/integration/modules/event.py
+++ tests/integration/modules/event.py
@@ -7,13 +7,15 @@
     :license: Apache 2.0, see LICENSE for more details.
 '''
 
+# Import python libs
 import time
 import threading
 from Queue import Queue, Empty
 
+# Import salt libs
 import integration
-
 from salt.utils import event
+
 
 class EventModuleTest(integration.ModuleCase):
     def test_event_fire_master(self):
@@ -26,7 +28,7 @@ class EventModuleTest(integration.ModuleCase):
             )
 
         threading.Thread(target=get_event, args=(events,)).start()
-        time.sleep(1)   # Allow multiprocessing.Process to start
+        time.sleep(3)   # Allow threading.Thread to start
 
         ret = self.run_function(
             'event.fire_master',
@@ -34,8 +36,11 @@ class EventModuleTest(integration.ModuleCase):
         )
         self.assertTrue(ret)
 
-        eventfired = events.get(block=True, timeout=1)
-        self.assertIn('event.fire_master: just test it!!!!', eventfired['data'])
+        eventfired = events.get(block=True, timeout=3)
+        self.assertIn(
+            'event.fire_master: just test it!!!!',
+            eventfired['data']
+        )
 
         ret = self.run_function(
             'event.fire_master',
@@ -44,7 +49,7 @@ class EventModuleTest(integration.ModuleCase):
         self.assertTrue(ret)
 
         with self.assertRaises(Empty):
-            eventfired = events.get(block=True, timeout=1)
+            eventfired = events.get(block=True, timeout=3)
 
     def test_event_fire(self):
         events = Queue()
@@ -56,7 +61,7 @@ class EventModuleTest(integration.ModuleCase):
             )
 
         threading.Thread(target=get_event, args=(events,)).start()
-        time.sleep(1)   # Allow multiprocessing.Process to start
+        time.sleep(3)   # Allow threading.Thread to start
 
         ret = self.run_function(
             'event.fire', ['event.fire: just test it!!!!', 'salttest']
@@ -72,7 +77,7 @@ class EventModuleTest(integration.ModuleCase):
         self.assertTrue(ret)
 
         with self.assertRaises(Empty):
-            eventfired = events.get(block=True, timeout=1)
+            eventfired = events.get(block=True, timeout=3)
 
     def test_event_fire_ipc_mode_tcp(self):
         events = Queue()
@@ -84,7 +89,7 @@ class EventModuleTest(integration.ModuleCase):
             )
 
         threading.Thread(target=get_event, args=(events,)).start()
-        time.sleep(1)   # Allow multiprocessing.Process to start
+        time.sleep(3)   # Allow threading.Thread to start
 
         ret = self.run_function(
             'event.fire', ['event.fire: just test it!!!!', 'salttest'],
@@ -102,7 +107,7 @@ class EventModuleTest(integration.ModuleCase):
         self.assertTrue(ret)
 
         with self.assertRaises(Empty):
-            eventfired = events.get(block=True, timeout=1)
+            eventfired = events.get(block=True, timeout=3)
 
 
 if __name__ == '__main__':
