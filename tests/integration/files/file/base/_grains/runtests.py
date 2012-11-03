2dfb66d9a4e857a75883db0d06fdb371177dd0da
Pedro Algarvio <pedro@algarvio.me>
2012-11-03 14:31:21 +0000

    This grains module helps "computing" some logic needed for runtests.

---
+++ tests/integration/files/file/base/_grains/runtests.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4 ts=4 fenc=utf-8
+"""
+    :copyright: © 2012 UfSoft.org - :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :license: Apache 2.0, see LICENSE for more details
+"""
+
+import sys
+
+
+runtests = {}
+grains = {'runtests': runtests}
+
+def __virtual__():
+    if __grains__.get('id') not in ('minion', 'sub_minion'):
+        return 'runtests'
+    return None
+
+
+def py_version_info():
+    runtests['py_version_info'] = sys.version_info
+
+
+def virtualenv_has_system_site_packages():
+    try:
+        import virtualenv
+    except ImportError:
+        return
+
+    try:
+        venv_version = virtualenv.__version__.split('.')
+    except AttributeError:
+        venv_version = virtualenv.virtualenv_version.split('.')
+
+    venv_version = tuple([int(i) for i in venv_version])
+
+    runtests['virtualenv_has_system_site_packages'] = venv_version >= (2, 7)
+    return grains
