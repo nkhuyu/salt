7c3ba3da5fcecf96371625481bcc2ef34570e425
Pedro Algarvio <pedro@algarvio.me>
2012-11-20 01:03:01 +0000

    GC and `.wait()` under 2.6

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -561,21 +561,28 @@ class ShellCase(TestCase):
         ppath = 'PYTHONPATH={0}:{1}'.format(CODE_DIR, ':'.join(sys.path[1:]))
         cmd = '{0} {1} {2} {3}'.format(ppath, PYEXEC, path, arg_str)
 
-        popen_kwargs = {
-            'shell': True,
-            'stdout': PIPE
-        }
-
         if catch_stderr:
-            popen_kwargs['stderr'] = PIPE
-
-        if not sys.platform.lower().startswith('win'):
-            popen_kwargs['close_fds'] = True
-
-        process = Popen(cmd, **popen_kwargs)
-
-        if catch_stderr:
-            out, err = process.communicate()
+            process = subprocess.Popen(
+                cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            if sys.version_info[0:2] < (2, 7):
+                # On python 2.6, the subprocess'es communicate() method uses
+                # select which, is limited by the OS to 1024 file descriptors
+                # We need more available descriptors to run the tests which
+                # need the stderr output.
+                # So instead of .communicate() we wait for the process to
+                # finish, but, as the python docs state "This will deadlock
+                # when using stdout=PIPE and/or stderr=PIPE and the child
+                # process generates enough output to a pipe such that it
+                # blocks waiting for the OS pipe buffer to accept more data.
+                # Use communicate() to avoid that." <- a catch, catch situation
+                #
+                # Use this work around were it's needed only, python 2.6
+                process.wait()
+                out = process.stdout.read()
+                err = process.stderr.read()
+            else:
+                out, err = process.communicate()
             # Force closing stderr/stdout to release file descriptors
             process.stdout.close()
             process.stderr.close()
@@ -588,6 +595,9 @@ class ShellCase(TestCase):
                     # process already terminated
                     pass
 
+        process = subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE
+        )
         data = process.communicate()
         process.stdout.close()
 
