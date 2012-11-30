fb11eccc783ffb430c36793cbd2226582ec3ce23
Pedro Algarvio <pedro@algarvio.me>
2012-11-30 13:24:46 +0000

    Show active multiprocessing children.

--- tests/saltunittest.py
+++ tests/saltunittest.py
@@ -12,6 +12,7 @@ import os
 import sys
 import logging
 import threading
+import multiprocessing
 
 # support python < 2.7 via unittest2
 if sys.version_info[0:2] < (2, 7):
@@ -59,43 +60,51 @@ def destructiveTest(func):
 from pympler import tracker
 MTRACKER = tracker.SummaryTracker()
 MTRACKER.print_diff()
-print 'Active Thread Count: {0}\n'.format(threading.active_count())
+print 'Active Thread Count: {0}'.format(threading.active_count())
+print 'Active multiprocessing children: {0}\n'.format(multiprocessing.active_children())
 
 
 class PymplerResultClass(TextTestResult):
     def startTest(self, test):
         MTRACKER.print_diff()
+        print 'Active multiprocessing children: {0}'.format(multiprocessing.active_children())
         print 'Active Thread Count: {0}\n'.format(threading.active_count())
         return super(PymplerResultClass, self).startTest(test)
 
     def addSuccess(self, test):
         super(PymplerResultClass, self).addSuccess(test)
         MTRACKER.print_diff()
+        print 'Active multiprocessing children: {0}'.format(multiprocessing.active_children())
         print 'Active Thread Count: {0}\n'.format(threading.active_count())
 
     def addError(self, test, err):
         super(PymplerResultClass, self).addError(test, err)
         MTRACKER.print_diff()
+        print 'Active multiprocessing children: {0}'.format(multiprocessing.active_children())
         print 'Active Thread Count: {0}\n'.format(threading.active_count())
 
     def addFailure(self, test, err):
         super(PymplerResultClass, self).addFailure(test, err)
         MTRACKER.print_diff()
+        print 'Active multiprocessing children: {0}'.format(multiprocessing.active_children())
         print 'Active Thread Count: {0}\n'.format(threading.active_count())
 
     def addSkip(self, test, reason):
         super(PymplerResultClass, self).addSkip(test, reason)
         MTRACKER.print_diff()
+        print 'Active multiprocessing children: {0}'.format(multiprocessing.active_children())
         print 'Active Thread Count: {0}\n'.format(threading.active_count())
 
     def addExpectedFailure(self, test, err):
         super(TextTestResult, self).addExpectedFailure(test, err)
         MTRACKER.print_diff()
+        print 'Active multiprocessing children: {0}'.format(multiprocessing.active_children())
         print 'Active Thread Count: {0}\n'.format(threading.active_count())
 
     def addUnexpectedSuccess(self, test):
         super(TextTestResult, self).addUnexpectedSuccess(test)
         MTRACKER.print_diff()
+        print 'Active multiprocessing children: {0}'.format(multiprocessing.active_children())
         print 'Active Thread Count: {0}\n'.format(threading.active_count())
 
 
