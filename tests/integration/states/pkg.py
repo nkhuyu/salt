96bca0b13508273e0d39e537f179a11f5b818875
Pedro Algarvio <pedro@algarvio.me>
2013-01-24 17:20:46 +0000

    Add test case to try and trigger #3420.

---
+++ tests/integration/states/pkg.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+    tests.integration.states.pkg
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
+'''
+
+# Import python linbs
+import os
+
+# Import salt libs
+import integration
+from saltunittest import skipIf
+
+
+class PkgStateTest(integration.ModuleCase, integration.SaltReturnAssertsMixIn):
+
+    @skipIf(os.geteuid() is not 0, 'you must be root to run this test')
+    def test_issue_3420_pkg_installed_and_cmdmod_is_windows(self):
+        template_lines = [
+            'testcase-packages:',
+            '  pkg.latest:',
+            '    - pkgs:',
+            '      - screen'
+        ]
+        template = '\n'.join(template_lines)
+        try:
+            ret = self.run_function('state.template_str', [template])
+            print 77778888899999, ret
+            self.assertSaltFalseReturn(ret)
+        except AssertionError:
+            raise
